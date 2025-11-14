22d109a4924e2c4161e03a8bb18c2c7ecc78e105
Mehnaj <tan.mehnaj@gmail.com>
2025-11-14 11:53:16 +0600

    done post method for add habit

--- index.js
+++ index.js
@@ -16,6 +16,27 @@ admin.initializeApp({
 app.use(cors());
 app.use(express.json());
 
+
+const verifyFirebaseToken = async(req,res,next)=>{
+  if(!req.headers.authorization){
+    return res.status(401).send({message: "unauthorized access"});
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  console.log('token',token);
+  if(!token){
+    return res.status(401).send({message: "unauthorized access"});
+  }
+
+  try{
+    const decoded = await admin.auth().verifyIdToken(token);
+    req.token_email = decoded.email;
+    console.log('from try in verify function')
+    next();
+  }catch(err){
+    return res.status(401).send({message: "unauthorized access"});
+  }
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@customcluster.mlvrouu.mongodb.net/?appName=CustomCluster`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,6 +58,35 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const db = client.db("habit_tracker");
+    const habitCollection = db.collection("habits");
+
+    //habits related apis
+    
+    //get all habits
+    app.get("/habits",async(req,res)=>{
+      const cursor = habitCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    //get lastest habits
+    app.get("/latestHabits",async(req,res)=>{
+      const cursor = habitCollection.find().sort({createdAt: -1}).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    // post habit
+    app.post("/habits",verifyFirebaseToken,async(req,res)=>{
+      const newHabit = req.body;
+      console.log('from post habit')
+      // const email = req.body.email;
+      // if(email !== req.token_email){
+      //   res.status(403).send({message: "forbidden access"});
+      // }
+      const result = await habitCollection.insertOne(newHabit);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
